2e0db0c012fbfb2f59b5a55e9c49f61ac9b85cf2
David Hartman <david.j.hartman@gmail.com>
2023-02-03 21:13:10 -0500

    Updated comment

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Django's command-line utility for administrative tasks.  Update made locally."""
 import os
 import sys
 
